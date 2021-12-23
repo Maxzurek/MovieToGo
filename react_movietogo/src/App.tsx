cf462de87e99c7e00342eed0c239a5b61be5f494
FHRAKATE <96500227+FHRAKATE@users.noreply.github.com>
2021-12-23 14:40:01 -0500

    Update App.tsx

--- react_movietogo/src/App.tsx
+++ react_movietogo/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from 'semantic-ui-react';
 import './App.css';
 
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button>ALLO</Button>
     </div>
   )
 };
