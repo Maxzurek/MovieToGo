68f2921b2b1cc30752ba8a980f66cd2b04f31d86
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-03 16:35:07 -0500

    Update UsersController.cs

--- MovieToGoAPI/MovieToGoAPI/Controllers/UsersController.cs
+++ MovieToGoAPI/MovieToGoAPI/Controllers/UsersController.cs
@@ -11,23 +11,43 @@ namespace MovieToGoAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly ILogger logger;
         private readonly UserManager<User> userManager;
         private readonly SignInManager<User> signInManager;
         private readonly IMapper mapper;
 
-        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        public UsersController(ILogger logger, UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
         {
+            this.logger = logger;
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.mapper = mapper;
         }
 
+        /// <summary>
+        /// Get all registered users
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public async Task<ActionResult<List<User>>> Get()
+        public async Task<ActionResult<List<UserDTO>>> Get()
         {
-            return await userManager.Users.ToListAsync();
+            logger.LogInformation("Getting all users");
+
+            var users = await userManager.Users.ToListAsync();
+
+            if (users.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return mapper.Map<List<UserDTO>>(users);
         }
 
+        /// <summary>
+        /// Create a user
+        /// </summary>
+        /// <param name="userRegistrationDTO"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("register")]
         [ProducesResponseType(typeof(IdentityResult), StatusCodes.Status400BadRequest)]
