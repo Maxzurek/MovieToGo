7a59a326462c6a270cae5fd39fdd50e0bc13c0f0
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-03 16:25:26 -0500

    Create UserDTO.cs

---
+++ MovieToGoAPI/MovieToGoAPI/DTOs/Users/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieToGoAPI.DTOs.Users
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTimeOffset LockoutEnd { get; set; }
+
+        public bool LockoutEnabled { get; set; }
+
+        public int AccessFailedCount { get; set; }
+
+        public string Discriminator { get; set; }
+
+    }
+}
