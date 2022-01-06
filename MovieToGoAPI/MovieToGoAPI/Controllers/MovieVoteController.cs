839e322add8d30ace5df48d3d535b6551219016e
mRDubeau <dubeau1992@gmail.com>
2022-01-05 19:08:53 -0500

    VoteController

--- MovieToGoAPI/MovieToGoAPI/Controllers/MovieVoteController.cs
+++ MovieToGoAPI/MovieToGoAPI/Controllers/MovieVoteController.cs
@@ -25,7 +25,7 @@ namespace MovieToGoAPI.Controllers
             this.mapper = mapper;
         }
         /// <summary>
-        /// Get all TheMovieDb vote
+        /// get all MovieVotes
         /// </summary>
         /// <returns></returns>
         [HttpGet]
@@ -44,6 +44,107 @@ namespace MovieToGoAPI.Controllers
 
             return mapper.Map<List<MovieVoteDTO>>(moviesVotes);
         }
+
+        /// <summary>
+        /// Get MovieVote by ID
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpGet("{Id:int}")]
+        [ProducesResponseType(typeof(MovieVoteDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<List<MovieVoteDTO>>> Get(int Id)
+        {
+            logger.LogInformation("Getting vote by Id");
+
+            MovieVote? movieVote = await context.MovieVotes.FirstOrDefaultAsync(x => x.Id == Id);
+            
+            if(movieVote == null)
+            {
+                return NotFound();
+
+            }
+
+            return mapper.Map<List<MovieVoteDTO>>(movieVote);
+            
+           
+        }
+
+        /// <summary>
+        /// Create MovieVote
+        /// </summary>
+        /// <param name="movieVoteCreationDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult> Post([FromBody] MovieVoteCreationDTO movieVoteCreationDTO)
+        {
+            logger.LogInformation("Creating a Vote");
+
+            MovieVote movieVote = mapper.Map<MovieVote>(movieVoteCreationDTO);
+
+            context.MovieVotes.Add(movieVote);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Update vote by VoteId
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="MovieVoteUpdateDTO"></param>
+        /// <returns></returns>
+        [HttpPut("{Id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult> Put(int Id, [FromBody] MovieVoteUpdateDTO MovieVoteUpdateDTO)
+        {
+            MovieVote movieVote = await context.MovieVotes.FirstOrDefaultAsync(x => x.Id == Id);
+
+           if(movieVote == null)
+            {
+                return NotFound();
+            }
+
+           movieVote = mapper.Map<MovieVote>(MovieVoteUpdateDTO);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        /// <summary>
+        /// Delete Vote by VoteId
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpDelete("{Id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult> Delete(int Id)
+        {
+            MovieVote? movieVote = await context.MovieVotes.FirstOrDefaultAsync(x => x.Id == Id);
+
+            if(movieVote == null)
+            {
+                return NotFound();
+            }
+            
+            context.MovieVotes.Remove(movieVote);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
 
     }
 }
---
+++ MovieToGoAPI/MovieToGoAPI/DTOs/MovieVotes/MovieVoteUpdateDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieToGoAPI.DTOs.MovieVotes
+{
+    public class MovieVoteUpdateDTO
+    {
+
+        public int Id { get; set; }
+        public int Vote{ get; set; }
+
+        
+
+    }
+}
