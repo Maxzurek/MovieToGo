883004c96181665354ebfc328b83ec4c516d0be4
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-08 02:15:54 -0500

    Update DisplayApiErrors.tsx

--- react_movietogo/src/components/utilities/DisplayApiErrors.tsx
+++ react_movietogo/src/components/utilities/DisplayApiErrors.tsx
@@ -2,61 +2,58 @@ import { AxiosError, AxiosResponse } from "axios";
 import { ReactElement, useEffect, useState } from "react";
 import { Container, Message } from "semantic-ui-react";
 
-interface ApiErrorsProps{
+interface ApiErrorsProps {
     error?: AxiosError;
 }
 
 export default function DisplayApiErrors(props: ApiErrorsProps) {
-    
+
     const [message, setMessage] = useState<ReactElement>(<></>);
 
 
-    const getError400MessageComponent = (response: AxiosResponse) : ReactElement => {
-        return(
+    const getError400MessageComponent = (response: AxiosResponse): ReactElement => {
+        return (
             <Message negative>
-                    <ul>{response.data.map((error: any, index: number) => <li key={index}>{error}</li>)}</ul>
-            </Message> 
+                <ul>{response.data.map((error: any, index: number) => <li key={index}>{error}</li>)}</ul>
+            </Message>
         )
     }
 
-    const getError500MessageComponent = () : ReactElement => {
+    const getError500MessageComponent = (): ReactElement => {
 
         const SERVER_ERR_MESSAGE = 'Internal Server Error, please try again later.'
 
-        return(
+        return (
             <Container textAlign="center">
                 <Message negative>{SERVER_ERR_MESSAGE}</Message>
             </Container>
         )
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
         setMessage(<></>)
 
-        if(props.error && props.error.response){
+        if (props.error && props.error.response) {
 
             const response = props.error.response as AxiosResponse;
 
-            switch(response.status){
+            switch (response.status) {
                 case 400: setMessage(getError400MessageComponent(response));
                     break;
                 default:
-                    break;                 
+                    break;
             }
         }
-        else if(props.error && props.error.response === undefined){
-            console.log(props.error);
+        else if (props.error && props.error.response === undefined) {
             setMessage(getError500MessageComponent());
         }
 
-
     }, [props.error])
-    
 
-    return(
+    return (
         <>
-            {props.error?.isAxiosError? message : undefined}
+            {props.error?.isAxiosError ? message : undefined}
         </>
     )
 };
