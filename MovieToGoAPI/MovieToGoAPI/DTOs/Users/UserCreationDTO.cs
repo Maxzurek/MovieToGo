f304f2761c5f770ecfe87299b2c3a37aa0999532
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-08 01:16:18 -0500

    Update UserCreationDTO.cs

--- MovieToGoAPI/MovieToGoAPI/DTOs/Users/UserCreationDTO.cs
+++ MovieToGoAPI/MovieToGoAPI/DTOs/Users/UserCreationDTO.cs
@@ -13,11 +13,9 @@ namespace MovieToGoAPI.DTOs.Users
         [Required(ErrorMessage = "The field {0} is required")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(150)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "The field {0} is required")]
         [StringLength(150)]
         public string LastName { get; set; }
     }
