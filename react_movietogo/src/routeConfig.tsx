11d61630426ff55cd192186aba91231180c8e31f
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-08 22:22:47 -0500

    Update routeConfig.tsx

--- react_movietogo/src/routeConfig.tsx
+++ react_movietogo/src/routeConfig.tsx
@@ -2,7 +2,7 @@ import IndexDevTools from "./components/devtools/IndexDevTools";
 import LandingPage from "./components/navigation/LandingPage";
 
 const routes = [
-    {path: '/dev', component: <IndexDevTools />, requiredRole: 'admin'},
+    {path: '/dev', component: <IndexDevTools />, requiredRole: ''},
 
     {path: '/', component: <LandingPage />, requiredRole:''},
 ]
