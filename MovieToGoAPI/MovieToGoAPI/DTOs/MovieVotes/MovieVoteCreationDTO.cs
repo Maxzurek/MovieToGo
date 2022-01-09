27a8fed88dc13659c0cefeafd44d182cb3ad88cc
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-09 15:34:30 -0500

    Update MovieVoteCreationDTO.cs

--- MovieToGoAPI/MovieToGoAPI/DTOs/MovieVotes/MovieVoteCreationDTO.cs
+++ MovieToGoAPI/MovieToGoAPI/DTOs/MovieVotes/MovieVoteCreationDTO.cs
@@ -12,9 +12,6 @@ namespace MovieToGoAPI.DTOs.MovieVotes
         public int Vote { get; set; }
 
         [Required(ErrorMessage = "The field {0} is required")]
-        public string UserId { get; set; }
-
-        [Required(ErrorMessage = "The field {0} is required")]
         public int MovieId { get; set; }
     }
 }
