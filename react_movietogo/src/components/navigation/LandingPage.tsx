6fe0fa156959d9ec5746f6ff7fbca775d0e20bd0
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-08 21:27:07 -0500

    Update LandingPage.tsx

--- react_movietogo/src/components/navigation/LandingPage.tsx
+++ react_movietogo/src/components/navigation/LandingPage.tsx
@@ -1,14 +1,9 @@
-import Authorized from "../authentication/Authorized";
+import { Container } from "semantic-ui-react";
 
 export default function LandingPage() {
     return (
-        <>
+        <Container textAlign="center">
             <h1>Landing page</h1>
-            <Authorized 
-                authorized={<>You are authorized</>} 
-                notAuthorized={<>You are not authorized</>}
-                role="admin"
-            />
-        </>
+        </Container>
     )
 };
