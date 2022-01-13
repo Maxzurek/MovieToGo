7357182565471474c154d332d44ce2688eb44d4e
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-12 20:34:07 -0500

    Update JeremyIndex.tsx

--- react_movietogo/src/components/navigation/JeremyIndex.tsx
+++ react_movietogo/src/components/navigation/JeremyIndex.tsx
@@ -1,11 +1,54 @@
-interface JeremyIndexProps{
+import axios from "axios"
+import { useState } from "react"
+import { Button, FormInput, Icon, Image, Input, InputOnChangeData, Segment } from "semantic-ui-react";
+import { theMovieDbImages, theMovieDbSearchByKeyword } from "../../endpoints"
+import GenericDataTable from "../utilities/GenericDataTable";
 
+interface JeremyIndexProps {
+}
+
+interface TheMovieDbDTO {
+    title: string;
+    release_date: string;
+    poster_path: string;
 }
 
 export default function JeremyIndex(props: JeremyIndexProps) {
-    return(
+
+    const [keyword, setKeyword] = useState('');
+    const [results, setResults] = useState<TheMovieDbDTO[]>([]);
+
+    const appelAPI = async () => {
+        let response = await axios.get(theMovieDbSearchByKeyword + keyword)
+        setResults(response.data.results);
+    }
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
+        setKeyword(data.value);
+        appelAPI();
+    }
+
+    return (
         <>
             <h3>Page jeremy</h3>
+            <Input onChange={onChange}></Input>
+            <Segment>
+                <ul>
+                    {results?.map((movie, index) => {
+                        console.log(movie);
+                        return (
+                            <>
+                                <Image size="tiny" src={theMovieDbImages + movie.poster_path} />
+                                <li key={index}>{movie.title} : {movie.release_date}</li>
+                            </>
+                        )
+                    })}
+                </ul>
+            </Segment>
+            <br />
+            <br />
+            <br />
+            <GenericDataTable url={theMovieDbSearchByKeyword + keyword} tableName="example" />
         </>
     )
 };
