09cef00d02cb2394b7c3586d1770c01497aa809e
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-14 12:47:51 -0500

    Update MovieVoteUpdateDTO.cs

--- MovieToGoAPI/MovieToGoAPI/DTOs/MovieVotes/MovieVoteUpdateDTO.cs
+++ MovieToGoAPI/MovieToGoAPI/DTOs/MovieVotes/MovieVoteUpdateDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace MovieToGoAPI.DTOs.MovieVotes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieToGoAPI.DTOs.MovieVotes
 {
     public class MovieVoteUpdateDTO
     {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, 5)]
         public int Vote{ get; set; }
     }
 }
