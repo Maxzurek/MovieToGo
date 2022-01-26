06482f10d00d54c718d9a4a626f7e530498d20d3
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-25 22:04:42 -0500

    Update MovieRating.tsx
    Fixed a bug when posting votes

--- react_movietogo/src/components/utilities/MovieRating.tsx
+++ react_movietogo/src/components/utilities/MovieRating.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse, AxiosTransformer } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import { Rating, RatingProps } from "semantic-ui-react";
 import { movieToGoUrlMovieVotes } from "../../endpoints";
@@ -8,6 +8,7 @@ import { MovieToGoDTO, MovieVoteDTO, MovieVoteUpdateDTO } from "../../models/mov
 interface movieRatingProps {
     movieToGoDTO: MovieToGoDTO | undefined;
 }
+
 export default function MovieRating(props: movieRatingProps) {
 
     const [rating, setRating] = useState<any>(1);
@@ -25,52 +26,57 @@ export default function MovieRating(props: movieRatingProps) {
 
         setRating(data.rating);
 
-        //add Rating
+        if (props.movieToGoDTO?.movieVote === undefined) {
+            postMovieVote(data.rating);
+        }
+        else {
+            updateMovieVote(data.rating)
+        }
+    }
+
+    const postMovieVote = async (rating: string | number | undefined) => {
+
         var movieVoteCreationDTO: MovieVoteDTO = {
             id: 0,
             vote: 0,
             user: undefined,
             movieId: 0
         }
+
+        movieVoteCreationDTO.movieId = props.movieToGoDTO?.id;
+
+        if (rating && typeof rating === 'number') {
+            movieVoteCreationDTO.vote = rating
+        }
+
+        await axios.post(movieToGoUrlMovieVotes, movieVoteCreationDTO)
+            .then((response: AxiosResponse<MovieVoteDTO>) => {
+                if (props.movieToGoDTO) {
+                    props.movieToGoDTO.movieVote = response.data;
+                }
+
+            })
+            .catch(error => console.log(error))
+    }
+
+    const updateMovieVote = async (rating: string | number | undefined) => {
+
         var movieVoteUpdateDTO: MovieVoteUpdateDTO = {
             vote: 0
         }
 
-        if (props.movieToGoDTO?.movieVote === undefined) {
-
-            movieVoteCreationDTO.movieId = props.movieToGoDTO?.id;
-
-            if (data.rating && typeof data.rating === 'number') {
-                movieVoteCreationDTO.vote = data.rating
-            }
-
-            //console.log(movieVoteCreationDTO)
-            await axios.post(movieToGoUrlMovieVotes, movieVoteCreationDTO)
-                .then((response: AxiosResponse<MovieVoteDTO>) => {
-
-                    if (props.movieToGoDTO?.movieVote) {
-                        props.movieToGoDTO.movieVote = response.data;
-                    }
-
-                })
-                .catch(error => console.log(error))
-
+        if (rating && typeof rating === 'number') {
+            movieVoteUpdateDTO.vote = rating
         }
-        else {
 
-            //Update Rating
-            if (data.rating && typeof data.rating === 'number') {
-                movieVoteUpdateDTO.vote = data.rating
-            }
-
-            await axios.put(movieToGoUrlMovieVotes + `/${props.movieToGoDTO.movieVote.id}`, { vote: data.rating })
-                .then(response => {
-                    if (props.movieToGoDTO?.movieVote) {
-                        props.movieToGoDTO.movieVote = response.data;
-                    }
-                })
-                .catch(error => console.log(error))
-        }
+        await axios.put(movieToGoUrlMovieVotes + `/${props?.movieToGoDTO?.movieVote?.id}`, movieVoteUpdateDTO)
+            .then(response => {
+                if (props.movieToGoDTO?.movieVote) {
+                    props.movieToGoDTO.movieVote = response.data;
+                    console.log(props.movieToGoDTO.movieVote)
+                }
+            })
+            .catch(error => console.log(error))
     }
 
     return (
@@ -82,5 +88,4 @@ export default function MovieRating(props: movieRatingProps) {
             rating={rating}
         />
     )
-
 };
