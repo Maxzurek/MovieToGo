1aed43082ca1971683e2f94f3f5d932e39f164d4
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-31 02:22:26 -0500

    Update WatchListItemContainer.tsx
    Now displaying a message when watchlist is empty

--- react_movietogo/src/components/watchlistindex/WatchListItemContainer.tsx
+++ react_movietogo/src/components/watchlistindex/WatchListItemContainer.tsx
@@ -1,47 +1,31 @@
-import axios from "axios";
-import { cloneElement, useContext, useEffect, useState } from "react";
-import { Button, Card, Container, Header, Segment } from "semantic-ui-react";
-import { movieToGoUrlWatchListItems, movieToGoUrlWatchLists, movieToGoUrlWatchListsUser } from "../../endpoints";
-import { MovieToGoDTO } from "../../models/movie.models";
+import { Container, Header } from "semantic-ui-react";
 import { WatchListDTO } from "../../models/watchlist.models";
-import { WatchListItemDTO } from "../../models/watchlist.models";
-import AppDataContext from "../contexts/AppDataContext";
 import IndividualMovie from "../utilities/IndividualMovie";
-import WatchListIndex from "./WatchListIndex";
-
 
 interface WatchListItemContainerProps {
     watchListDTO: WatchListDTO
 }
 
-
-
 export default function WatchListItemContainer(props: WatchListItemContainerProps) {
-
-
-
-
 
     return (
         <Container fluid>
-
-            {props.watchListDTO && props.watchListDTO.watchListItems ? props.watchListDTO.watchListItems.map((watchListItemDTO, index) => {
-                return (
-                    <IndividualMovie
-                        theMovieDbDTO={watchListItemDTO.theMovieDbDTO!}
-                        movieToGoDTO={watchListItemDTO.movie!}
-                        itemId={index.toString()}
-                        isInWatchList
-                        watchListItemID={watchListItemDTO.id}
-                        key={index}
-                        watchListId={props.watchListDTO.id}
-                    />
-                )
-            }) : undefined}
-
-
+            {props.watchListDTO && props.watchListDTO.watchListItems && props.watchListDTO.watchListItems.length > 0 ?
+                props.watchListDTO.watchListItems.map((watchListItemDTO, index) => {
+                    return (
+                        <IndividualMovie
+                            theMovieDbDTO={watchListItemDTO.theMovieDbDTO!}
+                            movieToGoDTO={watchListItemDTO.movie!}
+                            itemId={index.toString()}
+                            isInWatchList
+                            watchListItemID={watchListItemDTO.id}
+                            key={index}
+                            watchListId={props.watchListDTO.id}
+                        />
+                    )
+                })
+                :  <Container fluid textAlign="center"><Header as='h1'>This watchlist is empty ...</Header></Container>
+            }
         </Container>
-
-
     );
 };        
