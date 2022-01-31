f03dd1e986a90ebe88918c2053b919138a5a27e2
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-31 01:43:49 -0500

    Update WatchListCreate.tsx

--- react_movietogo/src/components/watchlistindex/WatchListCreate.tsx
+++ react_movietogo/src/components/watchlistindex/WatchListCreate.tsx
@@ -1,23 +1,20 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { Container, Form, FormInput, FormProps, Header, Segment, Button, Label } from "semantic-ui-react";
 import { movieToGoUrlWatchLists } from "../../endpoints";
 import { WatchListCreationDTO, WatchListDTO } from "../../models/watchlist.models";
 import AppDataContext from "../contexts/AppDataContext";
 import ModalContext from "../contexts/ModalContext";
 
-
 interface WatchListCreateProps {
     setActiveItem: React.Dispatch<React.SetStateAction<number>>
 }
 
-
-
 export default function WatchListCreate(props: WatchListCreateProps) {
+
     const { userWatchListDTO, setUserWatchListDTO } = useContext(AppDataContext)
     const [name, setName] = useState('');
     const { displayOkMessage } = useContext(ModalContext)
-
 
     async function createWatchList() {
         const watchListCreationDTO: WatchListCreationDTO = { name: name }
@@ -30,13 +27,13 @@ export default function WatchListCreate(props: WatchListCreateProps) {
                 if (userWatchListDTO) {
                     watchListDTO = userWatchListDTO.slice()
                 }
+
                 watchListDTO.push(response.data)
                 setUserWatchListDTO(watchListDTO)
                 displayOkMessage("WatchList Created!")
 
-                if (userWatchListDTO) {
-                    props.setActiveItem(watchListDTO?.length - 1)
-                }
+                console.log(watchListDTO?.length)
+                props.setActiveItem(watchListDTO?.length - 1)
 
             })
             .catch((error: AxiosError) => {
@@ -46,27 +43,20 @@ export default function WatchListCreate(props: WatchListCreateProps) {
 
     const handleItemClick = (event: React.FormEvent<HTMLFormElement>, data: FormProps) => {
         createWatchList();
-        console.log(name)
-
     }
-
-
-
 
     return (
         <Container fluid>
-            <Segment color="blue" inverted>
+            <Segment color="teal" inverted>
                 <Header textAlign="center" size="huge">
-                    Create a WatchList
+                    Create a new Watchlist
                 </Header>
             </Segment>
             <Form onSubmit={handleItemClick}>
-                <label>WatchList Name: </label>
+                <label>Watchlist Name: </label>
                 <FormInput type="text" name="name" value={name} onChange={(e, data) => setName(data.value)} />
-                <Button inverted color="blue" type="submit">Add</Button>
+                <Button inverted color="teal" type="submit">Create</Button>
             </Form>
         </Container>
     )
 };
-
-
