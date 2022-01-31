4e9c0b8c1e4eb49e36845359164e293746fa6030
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-31 01:43:51 -0500

    Update WatchListIndex.tsx

--- react_movietogo/src/components/watchlistindex/WatchListIndex.tsx
+++ react_movietogo/src/components/watchlistindex/WatchListIndex.tsx
@@ -1,8 +1,8 @@
 
 import { useContext, useEffect, useState } from "react";
-import { Button, Container, Grid, GridColumn, GridRow, Header, Icon, Loader, Menu, MenuItem, Segment } from "semantic-ui-react";
+import { Button, Container, Dropdown, Grid, GridColumn, GridRow, Header, Icon, Loader, Menu, MenuItem, Segment } from "semantic-ui-react";
 import { MovieToGoDTO, TheMovieDbDTO } from "../../models/movie.models";
-import { WatchListDTO } from "../../models/watchlist.models";
+import { WatchListDTO, WatchListItemDTO } from "../../models/watchlist.models";
 import WatchListItemContainer from "./WatchListItemContainer";
 import WatchListCreate from "../watchlistindex/WatchListCreate";
 import AppDataContext from "../contexts/AppDataContext";
@@ -22,38 +22,20 @@ export default function WatchListIndex(props: WatchListIndexProps) {
 
     const { userWatchListDTO, setUserWatchListDTO } = useContext(AppDataContext)
     const [activeItem, setActiveItem] = useState(0);
-    const [selectedWatchListDTO, setSelectedWatchListDTO] = useState<WatchListDTO | undefined >(userWatchListDTO![0])
-    const [watchListWasCreated, setWatchListWasCreated] = useState(false)
+    const [selectedWatchListDTO, setSelectedWatchListDTO] = useState<WatchListDTO | undefined>(userWatchListDTO![0])
     const [isLoading, setIsLoading] = useState(false)
-    const [firstTimeLoading, setFirstTimeLoading] = useState(true)
 
     const menuItemsStyle = { padding: "13px 10px" }
 
     const handleItemClick = (e: any, data: any) => {
-        if (data.name === 'watchListCreation') {
-            setActiveItem(-1);
 
-        } else {
-            setActiveItem(data.index);
-            if (userWatchListDTO) {
-                setSelectedWatchListDTO(userWatchListDTO[data.index])
-            }
+        setActiveItem(data.index);
+
+        if (userWatchListDTO) {
+            setSelectedWatchListDTO(userWatchListDTO[data.index])
         }
     }
 
-    const renderSegment = () => {
-
-        if (activeItem >= 0) {
-            return (
-                <WatchListItemContainer watchListDTO={selectedWatchListDTO!} />
-            )
-        } else {
-
-            return (
-                <WatchListCreate setActiveItem={setActiveItem} />
-            )
-        }
-    }
     const renderMenuItems = () => {
         return (
             userWatchListDTO?.map((watchlist, index) => {
@@ -72,53 +54,52 @@ export default function WatchListIndex(props: WatchListIndexProps) {
         )
     }
 
-    const test = async () => {
-        userWatchListDTO?.map((watchList, watchListIndex) => {
-            watchList.watchListItems!.map(async (watchListItem, watchListItemIndex) => {
-                let requests = []
-
-            })
-        })
-    }
-
     const fetchTheMovieDbData = async () => {
-        setIsLoading(true)
-        userWatchListDTO?.map((watchList, watchListIndex) => {
-            watchList.watchListItems?.map(async (watchListItem, watchListItemIndex) => {
+
+        await Promise.all(userWatchListDTO!.map(async (watchList, watchListIndex) => {
+
+            await Promise.all(watchList.watchListItems!.map(async (watchListItem, watchListItemIndex) => {
+
                 await axios.get(`${theMovieDbMovie}/${watchListItem.movie?.theMovieDbId}?api_key=${theMovieDbApiKey}&language=en-US`)
                     .then((response: AxiosResponse<TheMovieDbDTO>) => {
-                        console.log(response.data)
-                        let watchList = userWatchListDTO[watchListIndex]
+
+                        let watchList = userWatchListDTO![watchListIndex]
                         let watchListItem = watchList?.watchListItems?.[watchListItemIndex]
 
                         if (watchListItem) {
                             watchListItem.theMovieDbDTO = response.data
-                            setIsLoading(false)
-                            
                         }
-
                     })
                     .catch(error => {
                         setIsLoading(false)
                     });
-
-            })
-        })
+            }))
+        }))
     }
 
-    useEffect( () => {
-        console.log(userWatchListDTO![0])
+    const fetchData = async () => {
+
+        setIsLoading(true)
+
+        await fetchTheMovieDbData()
+            .then(promise => {
+                setIsLoading(false)
+            })
+    }
+
+    useEffect(() => {
+
         if (userWatchListDTO && userWatchListDTO.length > 0) {
             renderMenuItems();
-            fetchTheMovieDbData();
+            fetchData();
         }
-        
+
     }, [userWatchListDTO])
 
 
     return (
         <NotifyDataChangedContext.Provider value={() => {
-            fetchTheMovieDbData();
+            fetchData();
         }}>
 
             <Grid padded >
@@ -132,17 +113,27 @@ export default function WatchListIndex(props: WatchListIndexProps) {
                                 index={-1}
                                 name='watchListCreation'
                                 active={activeItem === -1}
-                                onClick={handleItemClick}
+                                onClick={() => setActiveItem(-1)}
                             >
-                                <Icon name='plus' /> Create a WatchList
+                                <Icon name='plus' /> New Watchlist
                             </Button>
                         </Menu>
 
                     </GridColumn>
                     <GridColumn width={13}>
                         <Segment loading={isLoading}>
-
-                            {!isLoading && userWatchListDTO && userWatchListDTO.length > 0 || activeItem === -1 ? renderSegment() : <Container textAlign="center"><Header>You have no Watchlist</Header></Container>}
+                            {activeItem >= 0 && !isLoading ?
+                                <WatchListItemContainer watchListDTO={selectedWatchListDTO!} />
+                                :
+                                undefined}
+                            {!userWatchListDTO ?
+                                <Container fluid textAlign="center"><Header>You have no watchlist yet</Header></Container>
+                                :
+                                undefined}
+                            {activeItem == -1 ?
+                                <WatchListCreate setActiveItem={setActiveItem} />
+                                :
+                                undefined}
                         </Segment>
                     </GridColumn>
                 </GridRow>
