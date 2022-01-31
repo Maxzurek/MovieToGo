1a2fce800d6942901833bccf7810c507c4ec53de
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2022-01-30 20:01:50 -0500

    Update MovieCards.tsx

--- react_movietogo/src/components/LandingPage/MovieCards.tsx
+++ react_movietogo/src/components/LandingPage/MovieCards.tsx
@@ -23,7 +23,6 @@ export default function MovieCards(props: moviesCardsProps) {
                         movieToGoDTO={props.movieToGoDTO[index]}
                         watchListDTO={props.watchListDTO}
                         itemId={index.toString()}
-                        isInWatchList = {true}
                     />
                 ))}
             </ScrollMenu>
